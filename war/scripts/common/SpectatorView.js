4717088427192f70be2c4fe8cde71bd8be827641
Sam Schreiber <sam.schreiber@gmail.com>
2011-05-03 04:07:35 +0000

    Remove stray instances of "Lorem ipsum..." testing text.

--- war/scripts/common/SpectatorView.js
+++ war/scripts/common/SpectatorView.js
@@ -137,7 +137,7 @@ var SpectatorView = {
     this.midDiv.style.cssText = "position: relative; width: 100%; height: 100%";
     this.textDiv = document.createElement('div');    
     this.textDiv.style.cssText = "position: relative; width: 100%; height: 100%; display: none; ";
-    this.textDiv.innerHTML = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    this.textDiv.innerHTML = "";
     
     this.gameVizDiv = document.createElement('div');
     this.gameOldVizDiv = document.createElement('div');
