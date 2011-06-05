24b75ff23af845aead91e8a429dbbdb43f980c2d
Sam Schreiber <sam.schreiber@gmail.com>
2011-06-05 00:05:54 +0000

    Add a memcached layer to the GGP.org website, based on the repository server memcached layer.

--- src/ggp/website/GGP_WebsiteServlet.java
+++ src/ggp/website/GGP_WebsiteServlet.java
@@ -1,25 +1,17 @@
 package ggp.website;
 
 import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.servlet.http.*;
-
 @SuppressWarnings("serial")
-public class GGP_WebsiteServlet extends HttpServlet {
-    public void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws IOException {
-        String reqURI = req.getRequestURI();
-
-        if (req.getServerName().equals("kiosk.ggp.org")) {
-            resp.setStatus(301);
-            resp.setHeader("Location", "http://www.ggp.org/kiosk/");
-            return;
-        }
-        
+public class GGP_WebsiteServlet extends CachedStaticServlet {
+    @Override
+    protected byte[] getResponseBytesForURI(String reqURI) throws IOException {
         if (reqURI.equals("/docs")) reqURI += "/";
         if (reqURI.equals("/kiosk")) reqURI += "/";
         
@@ -27,60 +19,38 @@ public class GGP_WebsiteServlet extends HttpServlet {
             reqURI += "index.html";
         }
 
-        boolean writeAsBinary = false;        
-        if (reqURI.endsWith(".html")) {
-            resp.setContentType("text/html");
-        } else if (reqURI.endsWith(".xml")) {
-            resp.setContentType("application/xml");
-        } else if (reqURI.endsWith(".xsl")) {
-            resp.setContentType("application/xml");
-        } else if (reqURI.endsWith(".js")) {
-            resp.setContentType("text/javascript");   
-        } else if (reqURI.endsWith(".json")) {
-            resp.setContentType("text/javascript");
-        } else if (reqURI.endsWith(".png")) {
-            resp.setContentType("image/png");
-            writeAsBinary = true;
-        } else if (reqURI.endsWith(".ico")) {
-            resp.setContentType("image/png");
-            writeAsBinary = true;
+        if (reqURI.endsWith(".png") || reqURI.endsWith(".ico")) {
+            return readBinaryFile(new File(reqURI.substring(1)));
         } else {
-            resp.setContentType("text/plain");
-        }
-
-        try {
-            if (writeAsBinary) {
-                writeStaticBinaryPage(resp, reqURI.substring(1));
-            } else {
-                // Temporary limits on caching, for during development.
-                resp.setHeader("Cache-Control", "no-cache");
-                resp.setHeader("Pragma", "no-cache");
-                writeStaticTextPage(resp, reqURI.substring(1));
-            }
-        } catch (IOException e) {
-            resp.setStatus(404);
+            return readFile(new File(reqURI.substring(1))).getBytes();
         }
     }
 
-    public void writeStaticTextPage(HttpServletResponse resp, String theURI) throws IOException {
-        FileReader fr = new FileReader(theURI);
+    private static String readFile(File rootFile) throws IOException {
+        // Show contents of the file.                                        
+        FileReader fr = new FileReader(rootFile);
         BufferedReader br = new BufferedReader(fr);
-        StringBuffer response = new StringBuffer();
         
+        String response = "";
         String line;
         while( (line = br.readLine()) != null ) {
-            response.append(line + "\n");
+            response += line + "\n";
         }
-                
-        resp.getWriter().println(response.toString());
+        
+        return response;
     }
     
-    public void writeStaticBinaryPage(HttpServletResponse resp, String theURI) throws IOException {
-        InputStream in = new FileInputStream(theURI);
+    private static byte[] readBinaryFile(File rootFile) throws IOException {
+        InputStream in = new FileInputStream(rootFile);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        
+        // Transfer bytes from in to out
         byte[] buf = new byte[1024];
         while (in.read(buf) > 0) {
-            resp.getOutputStream().write(buf);
+            out.write(buf);
         }
-        in.close();        
+        in.close();
+        
+        return out.toByteArray();
     }
 }
