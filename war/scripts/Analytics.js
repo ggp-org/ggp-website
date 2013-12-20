f8356b77007700639cf11dfda3a412d49579ec69
samschreiber <sam.schreiber@gmail.com>
2013-12-19 21:20:44 -0800

    Add a survey to collect feedback about the GGP.org website.

--- war/scripts/Analytics.js
+++ war/scripts/Analytics.js
@@ -1,12 +1,18 @@
 "use strict";  
 
 var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-15489024-4']);
-  _gaq.push(['_setDomainName', '.ggp.org']);
-  _gaq.push(['_trackPageview']);
+_gaq.push(['_setAccount', 'UA-15489024-4']);
+_gaq.push(['_setDomainName', '.ggp.org']);
+_gaq.push(['_trackPageview']);
 
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+(function() {
+  var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+(function() {
+  var gcs = document.createElement('script'); gcs.type = 'text/javascript'; gcs.async = true;
+  gcs.src = '//survey.g.doubleclick.net/async_survey?site=rvcmi2ytg6h2djkz2wujenyrae';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcs, s);
+})();
