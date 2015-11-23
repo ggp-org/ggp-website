1b20b428a2beb388bbb6364ff9c3980f928a9b94
samschreiber <sam.schreiber@gmail.com>
2015-11-23 01:18:01 -0800

    For tournaments where extra data from Tiltyard is available, display that extra data.

--- war/scripts/common/UserInterface.js
+++ war/scripts/common/UserInterface.js
@@ -151,6 +151,11 @@ var UserInterface = {
 
       return hV + mV + sV;
   },
+  
+  renderYesNo: function (b) {
+	  if (b) return "Yes";
+	  return "No";
+  },
     
   renderDateTime: function(d) {
     var monthShortNames = {0:"Jan",1:"Feb",2:"Mar",3:"Apr",4:"May",5:"Jun",6:"Jul",7:"Aug",8:"Sep",9:"Oct",10:"Nov",11:"Dec"};
